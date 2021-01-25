a491cdd05c1141f2f52b7150d3d8402752e538cc
Hakan Ensari <me@hakanensari.com>
2021-01-25 12:50:29 +0000

    Avoid downtime when updating

--- ingest.sh
+++ ingest.sh
@@ -1,27 +1,81 @@
 #!/bin/bash
 
-truncate_tables()
+create_tables ()
+{
+	# Some datasets come with apparently-incorrect fractional values for volume,
+	# so for now, I'm defaulting volume to numeric type.
+	psql $database <<-SQL
+		DROP TABLE IF EXISTS stocks_new, etfs_new, indices_new;
+		CREATE TABLE IF NOT EXISTS stocks_new (
+			symbol TEXT,
+			datetime TIMESTAMP,
+			open NUMERIC NOT NULL,
+			high NUMERIC NOT NULL,
+			low NUMERIC NOT NULL,
+			close NUMERIC NOT NULL,
+			volume NUMERIC NOT NULL
+		);
+		CREATE TABLE IF NOT EXISTS etfs_new (
+			symbol TEXT,
+			datetime TIMESTAMP,
+			open NUMERIC NOT NULL,
+			high NUMERIC NOT NULL,
+			low NUMERIC NOT NULL,
+			close NUMERIC NOT NULL,
+			volume NUMERIC NOT NULL
+		);
+		CREATE TABLE IF NOT EXISTS indices_new (
+			symbol TEXT,
+			datetime TIMESTAMP,
+			open NUMERIC NOT NULL,
+			high NUMERIC NOT NULL,
+			low NUMERIC NOT NULL,
+			close NUMERIC NOT NULL
+		);
+	SQL
+}
+
+# This extra step cleans duplicate rows FirstRateData seems to occasionally
+# introduce into its flat files.
+delete_duplicates ()
 {
 	for table in stocks etfs indices; do
-		psql -c "TRUNCATE $table" $database
+		psql $database <<-SQL
+			DELETE FROM ${table}_new a USING (
+				SELECT MIN(ctid) as ctid, symbol, datetime
+					FROM ${table}_new
+					GROUP BY symbol, datetime
+					HAVING COUNT(*) > 1
+				) b
+				WHERE a.symbol = b.symbol
+				AND a.datetime = b.datetime
+				AND a.ctid <> b.ctid
+		SQL
 	done
 }
 
-drop_indices()
+
+add_indices ()
 {
 	for table in stocks etfs indices; do
-		psql -c "DROP INDEX IF EXISTS ${table}_symbol_datetime_key" $database
+		psql -c "CREATE UNIQUE INDEX ${table}_new_symbol_datetime_key ON ${table}_new (symbol, datetime)" $database
 	done
 }
 
-add_indices()
+rename_tables ()
 {
 	for table in stocks etfs indices; do
-		psql -c "CREATE UNIQUE INDEX ${table}_symbol_datetime_key ON $table (symbol, datetime)" $database
+		psql $database <<-SQL
+			DROP TABLE IF EXISTS ${table}_old;
+			ALTER TABLE IF EXISTS $table RENAME TO ${table}_old;
+			ALTER TABLE ${table}_new RENAME TO $table;
+			ALTER INDEX IF EXISTS ${table}_symbol_datetime_key RENAME TO ${table}_old_symbol_datetime_key;
+			ALTER INDEX ${table}_new_symbol_datetime_key RENAME TO ${table}_symbol_datetime_key;
+		SQL
 	done
 }
 
-download_files()
+download_files ()
 {
 	# Don't download again if files are already downloaded
 	if [ `ls -1 *.zip 2>/dev/null | wc -l` == 0 ]; then
@@ -35,7 +89,7 @@ download_files()
 	fi
 }
 
-import_datasets()
+import_datasets ()
 {
 	while read collection; do
 		if [[ $collection =~ us_1500 ]]; then
@@ -63,31 +117,12 @@ import_datasets()
 			| sed "s/\r//" \
 			| sed "/^$/d" \
 			| sed "s/^/$symbol,/" \
-			| psql -c "COPY $table FROM STDIN WITH (FORMAT CSV)" $database
+			| psql -c "COPY ${table}_new FROM STDIN WITH (FORMAT CSV)" $database
 		done < <(unzip -Z1 $collection)
 	done < <(find . -name "*.zip")
 }
 
-# This extra step cleans duplicate rows FirstRateData seems to occasionally
-# introduce into its flat files.
-delete_duplicates()
-{
-	for table in stocks etfs indices; do
-		psql $database <<-SQL
-			DELETE FROM $table a USING (
-				SELECT MIN(ctid) as ctid, symbol, datetime
-					FROM $table
-					GROUP BY symbol, datetime
-					HAVING COUNT(*) > 1
-				) b
-				WHERE a.symbol = b.symbol
-				AND a.datetime = b.datetime
-				AND a.ctid <> b.ctid
-		SQL
-	done
-}
-
-usage()
+usage ()
 {
 	echo "usage: ingest [web] [database]"
 }
@@ -100,8 +135,8 @@ if [[ -z $web || -z $database || -n $3 ]]; then
 fi
 
 download_files
-truncate_tables
-drop_indices
+create_tables
 import_datasets
 delete_duplicates
 add_indices
+rename_tables
