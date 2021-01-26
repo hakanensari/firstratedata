7986219d9e76e01193757f394d62d31133e2dfa6
Hakan Ensari <me@hakanensari.com>
2021-01-26 06:47:05 +0000

    Improve print_count
    - Fix naming
    - Increment separately for each symbol

--- export-to-atrader.sql
+++ export-to-atrader.sql
@@ -15,35 +15,33 @@ with opens as (
 select extract(epoch
                from (datetime - {{date}}::timestamp))::integer as printtime,
        symbol as name,
-       row_number() over () as printcount,
-                         'NSDQ' as primary_exchange_name,
-                         'NSDQ' as exec_exchange_name,
+       (row_number() over (partition by symbol)) as print_count,
+       'NSDQ' as primary_exchange_name,
+       'NSDQ' as exec_exchange_name,
        "open" as last,
-                                 0 as size,
-                                 volume as print_size,
+       0 as size,
+       volume as print_size,
 
     (select "open"
      from opens
      where symbol = assets.symbol
      limit 1) as xopen,
-                                 0 as avgprice,
-                                 0 as bid,
-                                 0 as ask,
-                                 0 as pclose,
-                                 ' ' as sale_conditions,
-                                 0 as print_filter
+       0 as avgprice,
+       0 as bid,
+       0 as ask,
+       0 as pclose,
+       ' ' as sale_conditions,
+       0 as print_filter
 from (
           (select *
            from stocks
            where symbol = any(regexp_split_to_array(upper({{symbols}}), ',\s*'))
                and datetime::date = {{date}}
-               and datetime >= {{date}}::timestamp + 34200 * interval '1 second'
-           order by datetime)
+               and datetime >= {{date}}::timestamp + 34200 * interval '1 second' )
       union all
           (select *
            from etfs
            where symbol = any(regexp_split_to_array(upper({{symbols}}), ',\s*'))
                and datetime::date = {{date}}
-               and datetime >= {{date}}::timestamp + 34200 * interval '1 second'
-           order by datetime)
-      order by datetime) as assets
+               and datetime >= {{date}}::timestamp + 34200 * interval '1 second' )) as assets
+order by printtime
