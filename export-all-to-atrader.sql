2fd2f45d369e17175fadb52f9fb04ebb8b06ad54
Hakan Ensari <me@hakanensari.com>
2021-01-27 15:18:41 +0000

    Export all price points to ATrader

---
+++ export-all-to-atrader.sql
@@ -0,0 +1,56 @@
+with daily_snapshot as (
+    select *
+    from assets
+    where symbol = any(regexp_split_to_array(upper({{ symbols }}), ',\s*'))
+        and datetime::date = {{ date }}
+        and datetime >= {{ date }}::timestamp + 34200 * interval '1 second'
+        and datetime <= {{ date }}::timestamp + 57600 * interval '1 second'
+),
+ticks as (
+    select extract(epoch from (datetime - {{ date }}::timestamp))::integer as printtime,
+        symbol as name,
+        open as last,
+        volume as print_size
+    from daily_snapshot
+    union all
+    select extract(epoch from (datetime - {{ date }}::timestamp))::integer + 15 as printtime,
+        symbol as name,
+        case when open > close then high else low end as last,
+        volume as print_size
+    from daily_snapshot
+    union all
+    select extract(epoch from (datetime - {{ date }}::timestamp))::integer + 30 as printtime,
+        symbol as name,
+        case when open > close then low else high end as last,
+        volume as print_size
+    from daily_snapshot
+    union all
+    select extract(epoch from (datetime - {{ date }}::timestamp))::integer + 45 as printtime,
+        symbol as name,
+        close as last,
+        volume as print_size
+    from daily_snapshot
+)
+select printtime,
+    name,
+    (row_number() over (partition by name order by printtime)) as print_count,
+    'NSDQ' as primary_exchange_name,
+    'NSDQ' as exec_exchange_name,
+    last,
+    0 as size,
+    print_size,
+    (
+        select open
+        from daily_snapshot
+        where symbol = name
+            and datetime >= {{ date }}::timestamp + 34200 * interval '1 second'
+        limit 1
+    ) as xopen,
+    0 as avgprice,
+    0 as bid,
+    0 as ask,
+    0 as pclose,
+    ' ' as sale_conditions,
+    0 as print_filter
+from ticks
+order by printtime, name
--- export-open-to-atrader.sql
+++ export-open-to-atrader.sql
@@ -1,34 +1,29 @@
--- TODO: We'll need to add more logic to set xopens for stocks that don't open
--- at 9:30AM.
-with opens as
-    (select symbol,
-            "open"
-     from assets
-     where symbol = any(regexp_split_to_array(upper({{symbols}}), ',\s*'))
-         and datetime = {{date}}::timestamp + 34200 * interval '1 second' )
-select extract(epoch
-               from (datetime - {{date}}::timestamp))::integer as printtime,
-       symbol as name,
-       (row_number() over (partition by symbol)) as print_count,
-       'NSDQ' as primary_exchange_name,
-       'NSDQ' as exec_exchange_name,
-       "open" as last,
-       0 as size,
-       volume as print_size,
+-- TODO: We'll need logic to set xopens for stocks that don't open at 9:30AM?
 
-    (select "open"
-     from opens
-     where symbol = assets.symbol
-     limit 1) as xopen,
-       0 as avgprice,
-       0 as bid,
-       0 as ask,
-       0 as pclose,
-       ' ' as sale_conditions,
-       0 as print_filter
+select extract(epoch from (datetime - {{ date }}::timestamp))::integer as printtime,
+    symbol as name,
+    (row_number() over (partition by symbol)) as print_count,
+    'NSDQ' as primary_exchange_name,
+    'NSDQ' as exec_exchange_name,
+    open as last,
+    0 as size,
+    volume as print_size,
+    (
+        select open
+        from assets t1
+        where symbol = assets.symbol
+            and datetime >= {{ date }}::timestamp + 34200 * interval '1 second'
+        limit 1
+    ) as xopen,
+    0 as avgprice,
+    0 as bid,
+    0 as ask,
+    0 as pclose,
+    ' ' as sale_conditions,
+    0 as print_filter
 from assets
-where symbol = any(regexp_split_to_array(upper({{symbols}}), ',\s*'))
-    and datetime::date = {{date}}
-    and datetime >= {{date}}::timestamp + 34200 * interval '1 second'
-    and datetime <= {{date}}::timestamp + 57600 * interval '1 second'
+where symbol = any(regexp_split_to_array(upper({{ symbols }}), ',\s*'))
+    and datetime::date = {{ date }}
+    and datetime >= {{ date }}::timestamp + 34200 * interval '1 second'
+    and datetime <= {{ date }}::timestamp + 57600 * interval '1 second'
 order by printtime
