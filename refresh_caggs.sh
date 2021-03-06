dd264dc66b146b26d5fe42fd73d33ccc3c6d1f25
Hakan Ensari <me@hakanensari.com>
2021-03-06 19:00:44 +0000

    Don't materialise 1h bars

--- refresh_caggs.sh
+++ refresh_caggs.sh
@@ -2,7 +2,7 @@
 
 refresh_caggs ()
 {
-	for view in stocks_5m_rth etfs_5m_rth indexes_5m stocks_1h_rth etfs_1h_rth indexes_1h stocks_1d_rth etfs_1d_rth indexes_1d; do
+	for view in stocks_5m_rth etfs_5m_rth indexes_5m stocks_1d_rth etfs_1d_rth indexes_1d; do
 		psql -c "CALL refresh_continuous_aggregate('$view', NULL, NULL)" $database_url
 	done
 }
--- sql/002_create_views.sql
+++ sql/002_create_views.sql
@@ -75,69 +75,6 @@ WITH (
 	GROUP BY 1, 2
 WITH NO DATA;
 
-DROP MATERIALIZED VIEW IF EXISTS stocks_1h_rth CASCADE;
-
-CREATE MATERIALIZED VIEW stocks_1h_rth
-WITH (
-	timescaledb.continuous,
-	timescaledb.materialized_only = true
-) AS
-	SELECT
-		symbol,
-		time_bucket('1h', datetime) AS datetime,
-		first(open, datetime) AS open,
-		max(high) AS high,
-		min(low) AS low,
-		last("close", datetime) AS "close",
-		sum(volume) AS volume
-	FROM stocks_1m
-	WHERE EXTRACT(EPOCH FROM datetime::TIME) >= 34200
-	AND EXTRACT(EPOCH FROM datetime::TIME) <= 57600
-	AND datetime >= '2015-01-01'::DATE
-	GROUP BY 1, 2
-WITH NO DATA;
-
-DROP MATERIALIZED VIEW IF EXISTS etfs_1h_rth CASCADE;
-
-CREATE MATERIALIZED VIEW etfs_1h_rth
-WITH (
-	timescaledb.continuous,
-	timescaledb.materialized_only = true
-) AS
-	SELECT
-		symbol,
-		time_bucket('1h', datetime) AS datetime,
-		first(open, datetime) AS open,
-		max(high) AS high,
-		min(low) AS low,
-		last("close", datetime) AS "close",
-		sum(volume) AS volume
-	FROM etfs_1m
-	WHERE EXTRACT(EPOCH FROM datetime::TIME) >= 34200
-	AND EXTRACT(EPOCH FROM datetime::TIME) <= 57600
-	AND datetime >= '2015-01-01'::DATE
-	GROUP BY 1, 2
-WITH NO DATA;
-
-DROP MATERIALIZED VIEW IF EXISTS indexes_1h CASCADE;
-
-CREATE MATERIALIZED VIEW indexes_1h
-WITH (
-	timescaledb.continuous,
-	timescaledb.materialized_only = true
-) AS
-	SELECT
-		symbol,
-		time_bucket('1d', datetime) AS datetime,
-		first(open, datetime) AS open,
-		max(high) AS high,
-		min(low) AS low,
-		last("close", datetime) AS "close"
-	FROM indexes_1m
-	WHERE datetime >= '2015-01-01'::DATE
-	GROUP BY 1, 2
-WITH NO DATA;
-
 DROP MATERIALIZED VIEW IF EXISTS stocks_1d_rth CASCADE;
 
 CREATE MATERIALIZED VIEW stocks_1d_rth
@@ -221,13 +158,6 @@ CREATE OR REPLACE VIEW assets_5m_rth AS
 	UNION ALL
 	SELECT *
 	FROM etfs_5m_rth;
-
-CREATE OR REPLACE VIEW assets_1h_rth AS
-	SELECT *
-	FROM stocks_1h_rth
-	UNION ALL
-	SELECT *
-	FROM etfs_1h_rth;
 
 CREATE OR REPLACE VIEW assets_1d_rth AS
 	SELECT *
