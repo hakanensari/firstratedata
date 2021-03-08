928a035bb473a9376999bd33a85baf3e6b42d994
Hakan Ensari <me@hakanensari.com>
2021-03-08 14:22:57 +0000

    Remove optimisation

--- sql/functions/atrader_mav.sql
+++ sql/functions/atrader_mav.sql
@@ -6,7 +6,6 @@ CREATE OR REPLACE FUNCTION atrader_mav(text, timestamp, integer) RETURNS numeric
     	FROM assets_5m_rth
     	WHERE symbol = $1
 		AND datetime < CAST($2 AS timestamp) + interval '1 day'
-		AND datetime > CAST($2 AS timestamp) - interval '1 week' -- speeds up the query
 		ORDER BY datetime DESC
 		LIMIT $3
 	)
