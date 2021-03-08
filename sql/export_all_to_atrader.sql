66476c3a38252da1070cd85b99ea7fda3353a325
Hakan Ensari <me@hakanensari.com>
2021-03-08 22:44:47 +0000

    Add more data points to export
    This hacky SQL expands each minute bar to 15 ticks.

--- sql/export_all_to_atrader.sql
+++ sql/export_all_to_atrader.sql
@@ -6,24 +6,54 @@ WITH dataset AS (
 	AND datetime < {{datetime}} + interval '1 day'
 ), synthetic_dataset AS (
 	SELECT
-		EXTRACT(EPOCH FROM datetime::TIME) AS printtime,
+		EXTRACT(EPOCH FROM CAST(datetime AS time)) AS printtime,
 		symbol AS "name",
 		open AS "last",
-		(volume / 6)::INT AS print_size
+		(volume / 20)::INT AS print_size
 	FROM dataset
 	UNION ALL
 	SELECT
-		EXTRACT(EPOCH FROM datetime::TIME) + 10 AS printtime,
+		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 1) AS printtime,
 		symbol AS "name",
 		CASE
-			WHEN open > "close" THEN least(high, lag(high) OVER (PARTITION BY symbol ORDER BY datetime))
-			ELSE greatest(low, lag(low) OVER (PARTITION BY symbol ORDER BY datetime))
+			WHEN open > "close" THEN round(open + (high - open) * 1 / 5, 4)
+			ELSE round(open - (open - low) * 1 / 5, 4)
 		END AS "last",
-		(volume / 6)::INT AS print_size
+		CAST(volume / 16 AS int) AS print_size
 	FROM dataset
 	UNION ALL
 	SELECT
-		EXTRACT(EPOCH FROM datetime::TIME) + 20 AS printtime,
+		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 2) AS printtime,
+		symbol AS "name",
+		CASE
+			WHEN open > "close" THEN round(open + (high - open) * 2 / 5, 4)
+			ELSE round(open - (open - low) * 2 / 5, 4)
+		END AS "last",
+		CAST(volume / 16 AS int) AS print_size
+	FROM dataset
+	UNION ALL
+	SELECT
+		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 3) AS printtime,
+		symbol AS "name",
+		CASE
+			WHEN open > "close" THEN round(open + (high - open) * 3 / 5, 4)
+			ELSE round(open - (open - low) * 3 / 5, 4)
+		END AS "last",
+		CAST(volume / 16 AS int) AS print_size
+	FROM dataset
+	UNION ALL
+	SELECT
+		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 4) AS printtime,
+		symbol AS "name",
+		CASE
+			WHEN open > "close" THEN round(open + (high - open) * 4 / 5, 4)
+			ELSE round(open - (open - low) * 4 / 5, 4)
+		END AS "last",
+		CAST(volume / 16 AS int) AS print_size
+	FROM dataset
+	UNION ALL
+	SELECT
+		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 5) AS printtime,
 		symbol AS "name",
 		CASE
 			WHEN open > "close" THEN high
@@ -33,17 +63,47 @@ WITH dataset AS (
 	FROM dataset
 	UNION ALL
 	SELECT
-		EXTRACT(EPOCH FROM datetime::TIME) + 30 AS printtime,
+		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 6) AS printtime,
 		symbol AS "name",
 		CASE
-			WHEN open > "close" THEN greatest(low, lag(low) OVER (PARTITION BY symbol ORDER BY datetime))
-			ELSE least(high, lag(high) OVER (PARTITION BY symbol ORDER BY datetime))
+			WHEN open > "close" THEN round(high - (high - low) * 1 / 5, 4)
+			ELSE round(low + (high - low) * 1 / 5, 4)
 		END AS "last",
-		(volume / 6)::INT AS print_size
+		CAST(volume / 16 AS int) AS print_size
 	FROM dataset
 	UNION ALL
 	SELECT
-		EXTRACT(EPOCH FROM datetime::TIME) + 40 AS printtime,
+		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 7) AS printtime,
+		symbol AS "name",
+		CASE
+			WHEN open > "close" THEN round(high - (high - low) * 2 / 5, 4)
+			ELSE round(low + (high - low) * 2 / 5, 4)
+		END AS "last",
+		CAST(volume / 16 AS int) AS print_size
+	FROM dataset
+	UNION ALL
+	SELECT
+		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 8) AS printtime,
+		symbol AS "name",
+		CASE
+			WHEN open > "close" THEN round(high - (high - low) * 3 / 5, 4)
+			ELSE round(low + (high - low) * 3 / 5, 4)
+		END AS "last",
+		CAST(volume / 16 AS int) AS print_size
+	FROM dataset
+	UNION ALL
+	SELECT
+		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 9) AS printtime,
+		symbol AS "name",
+		CASE
+			WHEN open > "close" THEN round(high - (high - low) * 4 / 5, 4)
+			ELSE round(low + (high - low) * 4 / 5, 4)
+		END AS "last",
+		CAST(volume / 16 AS int) AS print_size
+	FROM dataset
+	UNION ALL
+	SELECT
+		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 10) AS printtime,
 		symbol AS "name",
 		CASE
 			WHEN open > "close" THEN low
@@ -53,10 +113,50 @@ WITH dataset AS (
 	FROM dataset
 	UNION ALL
 	SELECT
-		EXTRACT(EPOCH FROM datetime::TIME) + 50 AS printtime,
+		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 11) AS printtime,
+		symbol AS "name",
+		CASE
+			WHEN open > "close" THEN round(low + ("close" - low) * 1 / 5, 4)
+			ELSE round(high - (high - "close") * 1 / 5, 4)
+		END AS "last",
+		CAST(volume / 16 AS int) AS print_size
+	FROM dataset
+	UNION ALL
+	SELECT
+		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 12) AS printtime,
+		symbol AS "name",
+		CASE
+			WHEN open > "close" THEN round(low + ("close" - low) * 2 / 5, 4)
+			ELSE round(high - (high - "close") * 2 / 5, 4)
+		END AS "last",
+		CAST(volume / 16 AS int) AS print_size
+	FROM dataset
+	UNION ALL
+	SELECT
+		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 13) AS printtime,
+		symbol AS "name",
+		CASE
+			WHEN open > "close" THEN round(low + ("close" - low) * 3/ 5, 4)
+			ELSE round(high - (high - "close") * 3 / 5, 4)
+		END AS "last",
+		CAST(volume / 16 AS int) AS print_size
+	FROM dataset
+	UNION ALL
+	SELECT
+		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 14) AS printtime,
+		symbol AS "name",
+		CASE
+			WHEN open > "close" THEN round(low + ("close" - low) * 4 / 5, 4)
+			ELSE round(high - (high - "close") * 4 / 5, 4)
+		END AS "last",
+		CAST(volume / 16 AS int) AS print_size
+	FROM dataset
+	UNION ALL
+	SELECT
+		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 15) AS printtime,
 		symbol AS "name",
 		"close" AS "last",
-		(volume / 6)::INT AS print_size
+		CAST(volume / 16 AS int) AS print_size
 	FROM dataset
 )
 SELECT
