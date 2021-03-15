528c8078238b962888327362bddac4fdf239f1a8
Hakan Ensari <me@hakanensari.com>
2021-03-15 12:42:20 +0000

    Work around scraping errors

--- earnings/download.py
+++ earnings/download.py
@@ -2,12 +2,33 @@
 
 import csv
 import datetime
+import sys
+import traceback
+
+from workalendar.usa import UnitedStates
 from yahoo_earnings_calendar import YahooEarningsCalendar
 
-date_from = datetime.date(2015, 1, 4)
-date_to = datetime.date.today()
-earnings = YahooEarningsCalendar().earnings_between(date_from, date_to)
-filename = ("earnings-%s.csv" % date_to)
+from_date = datetime.date.fromisoformat(sys.argv[1])
+to_date = datetime.date.today()
+yec = YahooEarningsCalendar()
+cal = UnitedStates()
+
+earnings_data = []
+current_date = from_date
+delta = datetime.timedelta(days=1)
+
+while current_date <= to_date:
+    try:
+        if cal.is_working_day(current_date):
+            earnings_data += yec.earnings_on(current_date)
+    except:
+        traceback.print_exc()
+        break
+    print(current_date)
+    current_date += delta
+
+filename = ("earnings_%s.csv" % to_date)
+
 with open(filename, 'w') as file:
     fieldnames = earnings[0].keys()
     writer = csv.DictWriter(file, fieldnames=fieldnames)
