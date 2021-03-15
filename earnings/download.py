29fbc1b0d3199cb43cd3dcb9f74c5eb30f9b8334
Hakan Ensari <me@hakanensari.com>
2021-03-15 13:08:02 +0000

    Pause with exponential back-off if scraping fails

--- earnings/download.py
+++ earnings/download.py
@@ -3,6 +3,7 @@
 import csv
 import datetime
 import sys
+import time
 import traceback
 
 from workalendar.usa import UnitedStates
@@ -13,25 +14,33 @@ to_date = datetime.date.today()
 yec = YahooEarningsCalendar()
 cal = UnitedStates()
 
-earnings_data = []
+earnings = []
 current_date = from_date
 delta = datetime.timedelta(days=1)
+errors = 0
 
 while current_date <= to_date:
     try:
         if cal.is_working_day(current_date):
-            earnings_data += yec.earnings_on(current_date)
-    except:
-        traceback.print_exc()
-        break
-    print(current_date)
-    current_date += delta
+            earnings += yec.earnings_on(current_date)
+        current_date += delta
+        errors = 0
+        print(current_date)
+    except KeyError as e:
+        errors += 1
+        if errors > 9:
+            break
+
+        backoff_time = 2**errors - 1
+        time.sleep(backoff_time)
+        pass
 
 filename = ("earnings_%s.csv" % to_date)
 
 with open(filename, 'w') as file:
     fieldnames = earnings[0].keys()
     writer = csv.DictWriter(file, fieldnames=fieldnames)
+
     writer.writeheader()
     for row in earnings:
         writer.writerow(row)
