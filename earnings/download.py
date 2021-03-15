9e9afe2e8bf1e28b2970921cd3ebd01073d23224
Hakan Ensari <me@hakanensari.com>
2021-03-15 15:51:55 +0000

    Fix filename

--- earnings/download.py
+++ earnings/download.py
@@ -9,33 +9,38 @@ import traceback
 from workalendar.usa import UnitedStates
 from yahoo_earnings_calendar import YahooEarningsCalendar
 
-from_date = datetime.date.fromisoformat(sys.argv[1])
-to_date = datetime.date.today()
 yec = YahooEarningsCalendar()
 cal = UnitedStates()
 
-earnings = []
+from_date = datetime.date.fromisoformat(sys.argv[1])
+to_date = datetime.date.today()
 current_date = from_date
 delta = datetime.timedelta(days=1)
+earnings = []
 errors = 0
 
 while current_date <= to_date:
     try:
         if cal.is_working_day(current_date):
             earnings += yec.earnings_on(current_date)
+
         current_date += delta
         errors = 0
         print(current_date)
     except KeyError as e:
-        errors += 1
-        if errors > 9:
+        if errors > 6:
+            traceback.print_exc()
             break
 
+        errors += 1
         backoff_time = 2**errors - 1
         time.sleep(backoff_time)
         pass
+    except Exception as e:
+        traceback.print_exc()
+        break
 
-filename = ("earnings_%s.csv" % to_date)
+filename = f'earnings_{from_date}_{current_date}.csv'
 
 with open(filename, 'w') as file:
     fieldnames = earnings[0].keys()
