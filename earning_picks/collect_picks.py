66f944751d3f1cb38559d915c4ef234cbe897b73
Hakan Ensari <me@hakanensari.com>
2021-03-16 11:36:03 +0000

    Collect Misha's earning picks

---
+++ earning_picks/collect_picks.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import csv
+import datetime
+import glob
+import os
+import re
+import sys
+
+path = sys.argv[1]
+
+csvfile = open('earning_picks.csv', 'w')
+fieldnames = ['date', 'symbol']
+
+writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+writer.writeheader()
+
+for entry in os.scandir(path):
+    m = re.search(r'(\d{4})', entry.name)
+    year = m.group(1)
+
+    for entry in os.scandir(entry.path):
+        m = re.search(r'\.([A-Z][a-z]+)(\d+)', entry.name)
+
+        if m is None:
+            continue
+
+        month = m.group(1)
+        day = m.group(2)
+        if month == 'Jun':
+            month = 'June'
+
+        date = datetime.datetime.strptime(f'{year} {month} {day.zfill(2)}', "%Y %B %d").date()
+
+        path = glob.glob(f'{entry.path}/*.csv')[0]
+        csvfile = open(path)
+        reader = csv.reader(csvfile)
+
+        for row in reader:
+            if len(row) == 0:
+                continue
+
+            symbol = row[0]
+            if re.match(r'^[A-Z]+$', symbol) and symbol != 'DEFAULT':
+                writer.writerow({'date': date, 'symbol': symbol})
---
+++ earning_picks/create_earning_picks.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS earning_picks CASCADE;
+
+CREATE TABLE earning_picks (
+	date DATE NOT NULL,
+	symbol TEXT NOT NULL
+);
