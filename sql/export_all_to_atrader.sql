0513b9ff11cb6c0ce5198cad6980785305ee6ffb
Hakan Ensari <me@hakanensari.com>
2021-03-18 16:06:10 +0000

    Improve tick synthesis
    Now that I know data aggregators like FirstRateData ignore odd lots (orders less than 100 shares), we can make certain assumptions about our 1-minute data:
    
    - If volume is less than 199, there was only one order.
    
    - If volume is equal to or above 200 and less than 299, there were two orders.
    
    - If volume is equal to or above 300 and less than 399, there were three orders.
    
    - If volume is equal to or above 400, there were four or more orders.
    
    Above 400, I have added up to 14 more data points between open and close while making sure no single point has an odd lot.

--- sql/export_all_to_atrader.sql
+++ sql/export_all_to_atrader.sql
@@ -5,24 +5,48 @@ WITH dataset AS (
 	AND datetime >= {{datetime}}
 	AND datetime < CAST({{datetime}} AS timestamp) + interval '1 day'
 ),
-first_hour AS (
+dataset_with_volume_equal_to_or_above_1600 AS (
 	SELECT *
 	FROM dataset
-	WHERE EXTRACT(EPOCH FROM datetime::TIME) < 37800
+	WHERE volume >= 1600
 ),
-remaining_hours AS (
+dataset_with_volume_equal_to_or_above_600_and_below_1600 AS (
 	SELECT *
 	FROM dataset
-	WHERE EXTRACT(EPOCH FROM datetime::TIME) >= 37800
+	WHERE volume >= 600
+	AND volume < 1600
+),
+dataset_with_volume_equal_to_or_above_400_and_below_600 AS (
+	SELECT *
+	FROM dataset
+	WHERE volume >= 400
+	AND volume < 600
+),
+dataset_with_volume_equal_to_or_above_300_and_below_400 AS (
+	SELECT *
+	FROM dataset
+	WHERE volume >= 300
+	AND volume < 400
+),
+dataset_with_volume_equal_to_or_above_200_and_below_300 AS (
+	SELECT *
+	FROM dataset
+	WHERE volume >= 200
+	AND volume < 300
+),
+dataset_with_volume_below_200 AS (
+	SELECT *
+	FROM dataset
+	WHERE volume < 200
 ),
 synthetic_dataset AS (
-	-- first hour
+	-- volume >= 1600
 	SELECT
 		EXTRACT(EPOCH FROM CAST(datetime AS time)) AS printtime,
 		symbol AS "name",
 		open AS "last",
 		CAST(volume / 16 AS int) AS print_size
-	FROM first_hour
+	FROM dataset_with_volume_equal_to_or_above_1600
 	UNION ALL
 	SELECT
 		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 1) AS printtime,
@@ -32,7 +56,7 @@ synthetic_dataset AS (
 			ELSE round(open - (open - low) * 1 / 5, 4)
 		END AS "last",
 		CAST(volume / 16 AS int) AS print_size
-	FROM first_hour
+	FROM dataset_with_volume_equal_to_or_above_1600
 	UNION ALL
 	SELECT
 		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 2) AS printtime,
@@ -42,7 +66,7 @@ synthetic_dataset AS (
 			ELSE round(open - (open - low) * 2 / 5, 4)
 		END AS "last",
 		CAST(volume / 16 AS int) AS print_size
-	FROM first_hour
+	FROM dataset_with_volume_equal_to_or_above_1600
 	UNION ALL
 	SELECT
 		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 3) AS printtime,
@@ -52,7 +76,7 @@ synthetic_dataset AS (
 			ELSE round(open - (open - low) * 3 / 5, 4)
 		END AS "last",
 		CAST(volume / 16 AS int) AS print_size
-	FROM first_hour
+	FROM dataset_with_volume_equal_to_or_above_1600
 	UNION ALL
 	SELECT
 		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 4) AS printtime,
@@ -62,7 +86,7 @@ synthetic_dataset AS (
 			ELSE round(open - (open - low) * 4 / 5, 4)
 		END AS "last",
 		CAST(volume / 16 AS int) AS print_size
-	FROM first_hour
+	FROM dataset_with_volume_equal_to_or_above_1600
 	UNION ALL
 	SELECT
 		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 5) AS printtime,
@@ -72,7 +96,7 @@ synthetic_dataset AS (
 			ELSE low
 		END AS "last",
 		CAST(volume / 16 AS int) AS print_size
-	FROM first_hour
+	FROM dataset_with_volume_equal_to_or_above_1600
 	UNION ALL
 	SELECT
 		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 6) AS printtime,
@@ -82,7 +106,7 @@ synthetic_dataset AS (
 			ELSE round(low + (high - low) * 1 / 5, 4)
 		END AS "last",
 		CAST(volume / 16 AS int) AS print_size
-	FROM first_hour
+	FROM dataset_with_volume_equal_to_or_above_1600
 	UNION ALL
 	SELECT
 		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 7) AS printtime,
@@ -92,7 +116,7 @@ synthetic_dataset AS (
 			ELSE round(low + (high - low) * 2 / 5, 4)
 		END AS "last",
 		CAST(volume / 16 AS int) AS print_size
-	FROM first_hour
+	FROM dataset_with_volume_equal_to_or_above_1600
 	UNION ALL
 	SELECT
 		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 8) AS printtime,
@@ -102,7 +126,7 @@ synthetic_dataset AS (
 			ELSE round(low + (high - low) * 3 / 5, 4)
 		END AS "last",
 		CAST(volume / 16 AS int) AS print_size
-	FROM first_hour
+	FROM dataset_with_volume_equal_to_or_above_1600
 	UNION ALL
 	SELECT
 		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 9) AS printtime,
@@ -112,7 +136,7 @@ synthetic_dataset AS (
 			ELSE round(low + (high - low) * 4 / 5, 4)
 		END AS "last",
 		CAST(volume / 16 AS int) AS print_size
-	FROM first_hour
+	FROM dataset_with_volume_equal_to_or_above_1600
 	UNION ALL
 	SELECT
 		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 10) AS printtime,
@@ -122,7 +146,7 @@ synthetic_dataset AS (
 			ELSE high
 		END AS "last",
 		CAST(volume / 16 AS int) AS print_size
-	FROM first_hour
+	FROM dataset_with_volume_equal_to_or_above_1600
 	UNION ALL
 	SELECT
 		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 11) AS printtime,
@@ -132,7 +156,7 @@ synthetic_dataset AS (
 			ELSE round(high - (high - "close") * 1 / 5, 4)
 		END AS "last",
 		CAST(volume / 16 AS int) AS print_size
-	FROM first_hour
+	FROM dataset_with_volume_equal_to_or_above_1600
 	UNION ALL
 	SELECT
 		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 12) AS printtime,
@@ -142,7 +166,7 @@ synthetic_dataset AS (
 			ELSE round(high - (high - "close") * 2 / 5, 4)
 		END AS "last",
 		CAST(volume / 16 AS int) AS print_size
-	FROM first_hour
+	FROM dataset_with_volume_equal_to_or_above_1600
 	UNION ALL
 	SELECT
 		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 13) AS printtime,
@@ -152,7 +176,7 @@ synthetic_dataset AS (
 			ELSE round(high - (high - "close") * 3 / 5, 4)
 		END AS "last",
 		CAST(volume / 16 AS int) AS print_size
-	FROM first_hour
+	FROM dataset_with_volume_equal_to_or_above_1600
 	UNION ALL
 	SELECT
 		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 14) AS printtime,
@@ -162,23 +186,23 @@ synthetic_dataset AS (
 			ELSE round(high - (high - "close") * 4 / 5, 4)
 		END AS "last",
 		CAST(volume / 16 AS int) AS print_size
-	FROM first_hour
+	FROM dataset_with_volume_equal_to_or_above_1600
 	UNION ALL
 	SELECT
 		round(EXTRACT(EPOCH FROM CAST(datetime AS time)) + 3.75 * 15) AS printtime,
 		symbol AS "name",
 		"close" AS "last",
 		CAST(volume / 16 AS int) AS print_size
-	FROM first_hour
+	FROM dataset_with_volume_equal_to_or_above_1600
 
-	-- remaining hours
+	-- vol >= 600 and vol < 1600
 	UNION ALL
 	SELECT
 		EXTRACT(EPOCH FROM datetime::TIME) AS printtime,
 		symbol AS "name",
 		open AS "last",
 		CAST(volume / 6 AS int) AS print_size
-	FROM remaining_hours
+	FROM dataset_with_volume_equal_to_or_above_600_and_below_1600
 	UNION ALL
 	SELECT
 		EXTRACT(EPOCH FROM datetime::TIME) + 10 AS printtime,
@@ -188,7 +212,7 @@ synthetic_dataset AS (
 			ELSE greatest(low, lag(low) OVER (PARTITION BY symbol ORDER BY datetime))
 		END AS "last",
 		CAST(volume / 6 AS int) AS print_size
-	FROM remaining_hours
+	FROM dataset_with_volume_equal_to_or_above_600_and_below_1600
 	UNION ALL
 	SELECT
 		EXTRACT(EPOCH FROM datetime::TIME) + 20 AS printtime,
@@ -198,7 +222,7 @@ synthetic_dataset AS (
 			ELSE low
 		END AS "last",
 		CAST(volume / 6 AS int) AS print_size
-	FROM remaining_hours
+	FROM dataset_with_volume_equal_to_or_above_600_and_below_1600
 	UNION ALL
 	SELECT
 		EXTRACT(EPOCH FROM datetime::TIME) + 30 AS printtime,
@@ -208,7 +232,7 @@ synthetic_dataset AS (
 			ELSE least(high, lag(high) OVER (PARTITION BY symbol ORDER BY datetime))
 		END AS "last",
 		CAST(volume / 6 AS int) AS print_size
-	FROM remaining_hours
+	FROM dataset_with_volume_equal_to_or_above_600_and_below_1600
 	UNION ALL
 	SELECT
 		EXTRACT(EPOCH FROM datetime::TIME) + 40 AS printtime,
@@ -218,14 +242,101 @@ synthetic_dataset AS (
 			ELSE high
 		END AS "last",
 		CAST(volume / 6 AS int) AS print_size
-	FROM remaining_hours
+	FROM dataset_with_volume_equal_to_or_above_600_and_below_1600
 	UNION ALL
 	SELECT
 		EXTRACT(EPOCH FROM datetime::TIME) + 50 AS printtime,
 		symbol AS "name",
 		"close" AS "last",
 		CAST(volume / 6 AS int) AS print_size
-	FROM remaining_hours
+	FROM dataset_with_volume_equal_to_or_above_600_and_below_1600
+
+	-- vol >= 400 and vol < 600
+	UNION ALL
+	SELECT
+		EXTRACT(EPOCH FROM datetime::TIME) AS printtime,
+		symbol AS "name",
+		open AS "last",
+		CAST(volume / 4 AS int) AS print_size
+	FROM dataset_with_volume_equal_to_or_above_400_and_below_600
+	UNION ALL
+	SELECT
+		EXTRACT(EPOCH FROM datetime::TIME) + 15 AS printtime,
+		symbol AS "name",
+		CASE
+			WHEN open > "close" THEN high
+			ELSE low
+		END AS "last",
+		CAST(volume / 4 AS int) AS print_size
+	FROM dataset_with_volume_equal_to_or_above_400_and_below_600
+	UNION ALL
+	SELECT
+		EXTRACT(EPOCH FROM datetime::TIME) + 30 AS printtime,
+		symbol AS "name",
+		CASE
+			WHEN open > "close" THEN low
+			ELSE high
+		END AS "last",
+		CAST(volume / 4 AS int) AS print_size
+	FROM dataset_with_volume_equal_to_or_above_400_and_below_600
+	UNION ALL
+	SELECT
+		EXTRACT(EPOCH FROM datetime::TIME) + 45 AS printtime,
+		symbol AS "name",
+		"close" AS "last",
+		CAST(volume / 4 AS int) AS print_size
+	FROM dataset_with_volume_equal_to_or_above_400_and_below_600
+
+	-- vol >= 300 and vol < 400
+	UNION ALL
+	SELECT
+		EXTRACT(EPOCH FROM datetime::TIME) AS printtime,
+		symbol AS "name",
+		open AS "last",
+		CAST(volume / 3 AS int) AS print_size
+	FROM dataset_with_volume_equal_to_or_above_300_and_below_400
+	UNION ALL
+	SELECT
+		EXTRACT(EPOCH FROM datetime::TIME) + 20 AS printtime,
+		symbol AS "name",
+		CASE
+			WHEN low != open OR low != "close" THEN low
+			ELSE high
+		END AS "last",
+		CAST(volume / 3 AS int) AS print_size
+	FROM dataset_with_volume_equal_to_or_above_300_and_below_400
+	UNION ALL
+	SELECT
+		EXTRACT(EPOCH FROM datetime::TIME) + 40 AS printtime,
+		symbol AS "name",
+		"close" AS "last",
+		CAST(volume / 3 AS int) AS print_size
+	FROM dataset_with_volume_equal_to_or_above_300_and_below_400
+
+	-- vol >= 200 and vol < 300
+	UNION ALL
+	SELECT
+		EXTRACT(EPOCH FROM datetime::TIME) AS printtime,
+		symbol AS "name",
+		open AS "last",
+		CAST(volume / 2 AS int) AS print_size
+	FROM dataset_with_volume_equal_to_or_above_200_and_below_300
+	UNION ALL
+	SELECT
+		EXTRACT(EPOCH FROM datetime::TIME) + 30 AS printtime,
+		symbol AS "name",
+		"close" AS "last",
+		CAST(volume / 2 AS int) AS print_size
+	FROM dataset_with_volume_equal_to_or_above_200_and_below_300
+
+	-- vol < 200
+	UNION ALL
+	SELECT
+		EXTRACT(EPOCH FROM datetime::TIME) AS printtime,
+		symbol AS "name",
+		open AS "last",
+		CAST(volume AS int) AS print_size
+	FROM dataset_with_volume_below_200
 )
 SELECT
 	CAST(printtime AS int),
